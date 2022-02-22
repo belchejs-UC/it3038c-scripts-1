947523e8b0cca2f4424b2899d89768c5ce8a4e68
leonardf-git <leonardf@ucmail.uc.edu>
2022-02-22 16:23:54 -0500

    week7

---
+++ node/getIPbyHostname.js
@@ -0,0 +1,14 @@
+var dns = require("dns");
+
+function hostnameLookup(hostname) {
+	dns.lookup(hostname, function(err, addresses, family){
+		console.log(addresses);
+	});
+}
+
+
+
+var hostname = process.argv[2]
+
+console.log(`Checking IP of: ${hostname}`);
+hostnameLookup(hostname);
---
+++ node/test.js
@@ -0,0 +1,50 @@
+var path = require("path");
+
+
+
+console.log(`Process args: ${process.argv}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var hello = "Hello from NodeJs Variable"
+//console.log(`Printing from varaible: ${hello}`);
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log("Using the PATH module:");
+//console.log(`Hello from file ${path.basename(__filename)}`);
+
+
+
+
+//console.log("directory name: " + __dirname);
+//console.log("directory and file name: " + __filename);
+
+
+
+//console.log("Hello, world")
